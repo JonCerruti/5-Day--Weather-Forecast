df9e49f76570766082afb2b57ab31c9aa661cf56
Jon <jonathancerruti@gmail.com>
2022-07-07 15:13:33 -0700

    added to README for submission #1 still work to do to complete module

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # 5-Day--Weather-Forecast
+--- 
+
+# Objective
+The objective of this module was to create a 5 day weathr forecast using the open weather api. The webpage should show weather for an added city for 5 days and include temperature, humiduty, wind speed, and UV index. It should also save recent city searches on screen to be clicked on to go back to the search. 
+---
+
+# Tech Used 
+
+- HTML
+- CSS
+- Javascript
+- Github
+- VS Code
+- W3 Schools
+- Stack Overflow
+- Youtube
+- Bootstrap
+- Open Weather API
+
+![Screenshot]()
--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,34 @@
-function weatherBalloon( city ) {
-    var key = 'd8d1b7d44c89e722fd2bab6825b96f63';
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=city + &appid=' + key)  
+var enterCity = document.getElementById('enter-city')
+var searchBtn = document.getElementById('search-button')
+var clearHist = document.getElementById('clear-history')
+var hist = document.getElementById('history')
+var todayWeather = document.getElementById('today-weather')
+var cityName = document.getElementById('city-name')
+var currentCity = document.getElementById('current-pic')
+var temp = document.getElementById('temperature')
+var humidity = document.getElementById('humidity')
+var windSpeed = document.getElementById('wind-speed')
+var uVIndex = document.getElementById('UV-index')
+var fiveDayHead = document.getElementById('fiveday-header')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function weatherBalloon( cityID ) {
+    const key = 'd8d1b7d44c89e722fd2bab6825b96f63';
+    fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
     .then(function(resp) { return resp.json() }) // Convert data to json
     .then(function(data) {
       console.log(data);
@@ -11,5 +39,5 @@ function weatherBalloon( city ) {
   }
   
   window.onload = function() {
-    weatherBalloon(  );
+    weatherBalloon( 6167865 );
   }
